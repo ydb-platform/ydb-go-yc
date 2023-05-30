6dd9bb0d3a462b32425890e93df597c564e8ea45
Andry <Airmanhip@gmail.com>
2023-05-30 20:28:32 +0300

    Actualization of the example internal/cmd/connect/main.go

--- internal/cmd/connect/main.go
+++ internal/cmd/connect/main.go
@@ -14,9 +14,6 @@ func main() {
 	db, err := ydb.Open(
 		ctx,
 		"grpcs://ydb.serverless.yandexcloud.net:2135/?database=/ru-central1/b1g8skpblkos03malf3s/etnaeujopcre7mubi9lj",
-		// or define directly endpoint and database
-		//ydb.WithEndpoint("ydb.serverless.yandexcloud.net:2135"),
-		//ydb.WithDatabase("/ru-central1/b1g8skpblkos03malf3s/etnaeujopcre7mubi9lj"),
 
 		// credentials to access YDB outside yandex-cloud
 		yc.WithServiceAccountKeyFileCredentials(os.Getenv("YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS")),
